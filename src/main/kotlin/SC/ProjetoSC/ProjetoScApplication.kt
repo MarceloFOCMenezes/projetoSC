f38b4e0b5dc5c4b1fccd0307ec041bcf512c97c5
MarceloFOCMenezes <marcelo.menezes@sptech.school>
2025-03-07 17:36:00 -0300

    Adicionando projeto springboot

---
+++ src/main/kotlin/SC/ProjetoSC/ProjetoScApplication.kt
@@ -0,0 +1,11 @@
+package SC.ProjetoSC
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoScApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoScApplication>(*args)
+}
